408934605331faab7a10a9e50e4097f9cb3ec30d
Shamil Nakokhov <nakokhov@gmail.com>
2018-10-12 00:45:12 +0300

    assignment03 done

--- assignments/03/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/DataAnalysis.kt
+++ assignments/03/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/DataAnalysis.kt
@@ -9,11 +9,11 @@ enum class DataSource {
 }
 
 sealed class Profile(
-    var id: Long,
-    var firstName: String? = null,
-    var lastName: String? = null,
-    var age: Int? = null,
-    var dataSource: DataSource
+        var id: Long,
+        var firstName: String? = null,
+        var lastName: String? = null,
+        var age: Int? = null,
+        var dataSource: DataSource
 )
 
 /**
@@ -22,13 +22,40 @@ sealed class Profile(
  */
 class FacebookProfile(id: Long) : Profile(dataSource = DataSource.FACEBOOK, id = id)
 
+class LinkedinProfile(id: Long) : Profile(dataSource = DataSource.LINKEDIN, id = id)
+class VkProfile(id: Long) : Profile(dataSource = DataSource.VK, id = id)
+
+fun RawProfile.toProfile(id: Long): Profile? {
+    val dictData = rawData.split(",\n")
+            .map { it.split("=") }
+            .filter { it.size == 2 }
+            .associate { it[0] to it[1] }
+
+    return when (dictData["source"]) {
+        "vk" -> VkProfile(id)
+        "facebook" -> FacebookProfile(id)
+        "linkedin" -> LinkedinProfile(id)
+        else -> null
+    }?.apply {
+        firstName = dictData["fisrtName"]
+        lastName = dictData["lastName"]
+        age = dictData["age"]?.toIntOrNull()
+    }
+}
+
+
 /**
  * Task #2
  * Find the average age for each datasource (for profiles that has age)
  *
  * TODO
  */
-val avgAge: Map<DataSource, Double> = emptyMap()
+val avgAge: Map<DataSource, Double> = getAvgAge(getRawProfiles())
+
+fun getAvgAge(profiles: Collection<RawProfile>): Map<DataSource, Double> {
+    val dataSourceToProfile = makeProfiles(profiles).groupBy({ it.dataSource }, { it.age })
+    return dataSourceToProfile.map { it.key to it.value.filterNotNull().average() }.toMap()
+}
 
 /**
  * Task #3
@@ -37,63 +64,81 @@ val avgAge: Map<DataSource, Double> = emptyMap()
  *
  * TODO
  */
-val groupedProfiles: Map<Long, List<Profile>> = emptyMap()
+
+fun getGroupedProfiles(profiles: Collection<RawProfile>): Map<Long, List<Profile>> {
+    val profiles = makeProfiles(profiles)
+    val isSameProfile = { a: Profile, b: Profile ->
+        a.firstName?.capitalize() == b.firstName?.capitalize() &&
+                a.lastName?.capitalize() == b.lastName?.capitalize() &&
+                a.age == b.age
+    }
+    return profiles.associate { p -> p.id to profiles.filter { isSameProfile(p, it) } }
+}
+
+val groupedProfiles: Map<Long, List<Profile>> = getGroupedProfiles(getRawProfiles())
 
 /**
  * Here are Raw profiles to analyse
  */
-val rawProfiles = listOf(
-    RawProfile("""
+
+fun getRawProfiles() = listOf(
+        RawProfile("""
             fisrtName=alice,
             age=16,
             source=facebook
             """.trimIndent()
-    ),
-    RawProfile("""
+        ),
+        RawProfile("""
             fisrtName=Dent,
             lastName=kent,
             age=88,
             source=linkedin
             """.trimIndent()
-    ),
-    RawProfile("""
+        ),
+        RawProfile("""
             fisrtName=alla,
             lastName=OloOlooLoasla,
             source=vk
             """.trimIndent()
-    ),
-    RawProfile("""
+        ),
+        RawProfile("""
             fisrtName=bella,
             age=36,
             source=vk
             """.trimIndent()
-    ),
-    RawProfile("""
+        ),
+        RawProfile("""
             fisrtName=angel,
             age=I will not tell you =),
             source=facebook
             """.trimIndent()
-    ),
+        ),
 
-    RawProfile(
-        """
+        RawProfile(
+                """
             lastName=carol,
             source=vk
             age=49,
             """.trimIndent()
-    ),
-    RawProfile("""
+        ),
+        RawProfile("""
             fisrtName=bob,
             lastName=John,
             age=47,
             source=linkedin
             """.trimIndent()
-    ),
-    RawProfile("""
+        ),
+        RawProfile("""
             lastName=kent,
             fisrtName=dent,
             age=88,
             source=facebook
             """.trimIndent()
-    )
+        )
 )
+
+fun makeProfiles(source: Collection<RawProfile>): List<Profile> {
+    var id = 0L
+    @Suppress("UNCHECKED_CAST")
+    return source.map { it.toProfile(id)?.also { id++ } }.filter { it != null } as List<Profile>
+}
