999c6931e8379ea0d5a0da6aaab304f601c4dbef
Shamil Nakokhov <nakokhov@gmail.com>
2018-10-12 01:40:14 +0300

    Added tests

--- assignments/03/src/test/kotlin/io/rybalkinsd/kotlinbootcamp/DataAnalysisTest.kt
+++ assignments/03/src/test/kotlin/io/rybalkinsd/kotlinbootcamp/DataAnalysisTest.kt
@@ -14,4 +14,31 @@ class DataAnalysisTest {
     fun `check grouped profiles`() {
         assertTrue(groupedProfiles.isNotEmpty())
     }
+
+    @Test
+    fun `check grouped profiles are contain at least one element`() {
+        assertTrue(getGroupedProfiles(getRawProfiles()).all { !it.value.isEmpty() })
+    }
+
+    @Test
+    fun `check avg age2`() {
+        val rawData = listOf<RawProfile>(
+                RawProfile("""
+                    age=10,
+                    source=vk
+                """.trimIndent()),
+                RawProfile("""
+                    age=20,
+                    source=facebook
+                """.trimIndent()),
+                RawProfile("""
+                    age=30,
+                    source=linkedin
+                """.trimIndent())
+        )
+        val sourceToAvg = getAvgAge(rawData)
+        assertTrue((sourceToAvg[DataSource.VK] ?: 0) == 10.0)
+        assertTrue((sourceToAvg[DataSource.FACEBOOK] ?: 0) == 20.0)
+        assertTrue((sourceToAvg[DataSource.LINKEDIN] ?: 0) == 30.0)
+    }
 }
